f7d008aeadff4a492cddd129f70c85f9863009b1
Dylan Faulkner <dylanfaulkner616@gmail.com>
2026-09-25 20:10:05 +0100

    Created the dev branch

---
+++ build/CMakeFiles/SWE_Year3_Game.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SWE_Year3_Game.dir/main.cpp.o"
+  "CMakeFiles/SWE_Year3_Game.dir/main.cpp.o.d"
+  "SWE_Year3_Game"
+  "SWE_Year3_Game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SWE_Year3_Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
